2eb74e5a8bab5967ee246e6ee5db75fb9985dcab
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-08-22 15:09:33 -0400

    added instructions Nhu shared, thanks Nhu!

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+  let listedPlanetsResponse = myFetch();
+
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      //  const planet = pickPlanet()
+      //  addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
    })
-   
+
+
+   // get the form
+   // add a listener to when the form submit
+  //  form.addEventListener('submit', function(event) {
+      // if it's not, preventDefault
+      // get the value for each of the input field
+      // let pilotInput = this.document.querySelector("input[name=pilotName]");
+      // const pilotValue = pilotInput.value
+
+      // let list = document.getElementById('faultyItems');
+      // formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue)
+
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,42 +2,74 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+  // get the missionTarget div
+  // set the inner HTML to this
+  // fill in the information that is passed in
    // Here is the HTML formatting for our mission target div.
    /*
+    `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
+                    <li>Name: ${name}</li>
                     <li>Diameter: </li>
                     <li>Star: ${star}</li>
                     <li>Distance from Earth: </li>
                     <li>Number of Moons: </li>
                 </ol>
                 <img src="">
+                `
    */
 }
 
 function validateInput(testInput) {
-   
+  //check if the test Input is empty
+    // if it is, return 'Empty'
+  // check if it's not a number
+    // return 'Not a Number'
+  // else
+    // return 'Is a Number'
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue) {
+  // check if any of the values are empty
+    // if (validateInput(pilotValue) === 'Empty' || validateInput(copilotValue) === 'Empty')
+    // alert user that they need to fill out all the fields alert('message')
+  // check if fuelLevelValue and cargoLevelValue are not numbers
+    // alert the user that must enter valid input
+
+
+  // set the list.style.visibility = 'visible'
+  // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
+  // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
+  // check if the fuel level is less 10,000
+    // change launchStatus to "Shuttle not ready for launch", and color to red
+    // change the fuelStatus to "Fuel level too low for launch"
+
+    // check if the cargo level is more than 10,000
+    // change launchStatus to "Shuttle not ready for launch", and color to red
+    // change the cargoStatus to "Cargo level too high for launch"
+
+    // if both fuel and cargo are good
+      // change the launchStatus to "Shuttle is Ready for Launch" and color to green
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+  planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then(function (response) {
+      // get the json from the response
         });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+  // randomly pick a planet from the array
+  // Math random for index
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
