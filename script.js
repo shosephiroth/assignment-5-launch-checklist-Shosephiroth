1901c94ac8fc328aaebe28d5c19ba2d3dce6d2bc
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-02 18:01:45 -0400

    added a little code Nhu suggested

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener("load", function() {
 //        console.log(listedPlanets);
 
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-      //  const planet = pickPlanet()
+      //  const planet = pickPlanet(listedPlanets)
       //  addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
    //}) also part of commented out code before reaching myFetch()
 
