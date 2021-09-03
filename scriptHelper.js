19114aa669c1f7d6947d01bbf1a2efd90b95833e
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-02 22:11:52 -0400

    more fixes, getting number values alert dk why

--- scriptHelper.js
+++ scriptHelper.js
@@ -88,19 +88,19 @@ function formSubmission(
     // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
     // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
     let pilotStatus = document.getElementById("pilotStatus");
-    let copilotStatus = document.getElementById("copilotStatus");
     pilotStatus.innerHTML = `Pilot ${pilotValue} is ready for launch`;
+    let copilotStatus = document.getElementById("copilotStatus");
     copilotStatus.innerHTML = `CoPilot ${copilotValue} is ready for launch`;
     let launchStatus = document.getElementById("launchStatus");
     let fuelStatus = document.getElementById("fuelStatus");
-    // check if the fuel level is less 10,000
-    // change launchStatus to "Shuttle not ready for launch", and color to red
-    // change the fuelStatus to "Fuel level too low for launch"
     let cargoStatus = document.getElementById("cargoStatus");
     fuelStatus.innerHTML = "Fuel status good for launch!";
     cargoStatus.innerHTML = "Cargo status is good for launch!";
+    // check if the fuel level is less 10,000
+    // change launchStatus to "Shuttle not ready for launch", and color to red
+    // change the fuelStatus to "Fuel level too low for launch"
 
-    if (fuelLevelValue < 10000) {
+    if (Number(fuelLevelValue) < 10000) {
       list.style.visibility = "visible";
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
@@ -110,7 +110,7 @@ function formSubmission(
     // check if the cargo level is more than 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the cargoStatus to "Cargo level too high for launch"
-    if (cargoLevelValue > 10000) {
+    if (Number(cargoLevelValue) > 10000) {
       list.style.visibility = "visible";
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
@@ -119,7 +119,7 @@ function formSubmission(
 
     // if both fuel and cargo are good
     // change the launchStatus to "Shuttle is Ready for Launch" and color to green
-    if (fuelLevelValue < 10000 && cargoLevelValue > 10000) {
+    if (Number(fuelLevelValue) < 10000 && Number(cargoLevelValue) > 10000) {
       launchStatus.innerHTML = "Shuttle is Ready for Launch";
       launchStatus.style.color = "green";
     }
@@ -133,7 +133,7 @@ async function myFetch() {
     "https://handlers.education.launchcode.org/static/planets.json"
   ).then(function (response) {
     // get the json from the response
-    return response.json;
+    return response.json();
   });
 
   return planetsReturned;
