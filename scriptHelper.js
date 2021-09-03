67ed8d1c747c5265812573273f4caf79aa950173
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-02 21:04:36 -0400

    thanks Max for  your help!

--- scriptHelper.js
+++ scriptHelper.js
@@ -38,33 +38,43 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue) {
-  
-  // end of possibly useless code
-  
+
   // check if any of the values are empty
     // if (validateInput(pilotValue) === 'Empty' || validateInput(copilotValue) === 'Empty')
     // alert user that they need to fill out all the fields alert('message')
   // check if fuelLevelValue and cargoLevelValue are not numbers
     // alert the user that must enter valid input
 
-  if (validateInput(pilotValue.value) === 'Empty' || validateInput(copilotValue.value) === 'Empty' || validateInput(fuelLevelValue.value) === 'Empty' || validateInput(cargoLevelValue.value) === 'Empty') {
-    alert("All fields required to be filled!");
-    //preventDefault(); // event.preventDefault()? What is the event? Do I need to add to form.addEventListener?
+  if (validateInput(pilotValue) === 'Empty' || validateInput(copilotValue) === 'Empty' || validateInput(fuelLevelValue) === 'Empty' || validateInput(cargoLevelValue) === 'Empty') {
+    alert("All fields required to be filled!");    
   } else if (validateInput(fuelLevelValue) === "Not a Number" || validateInput(cargoLevelValue) === "Not a Number") {
-      alert("Must enter a Number for Fuel Level and Cargo Mass values");
-      //preventDefault();
-  };
-
+      alert("Must enter a Number for Fuel Level and Cargo Mass values");      
+  } else {
 
   // set the list.style.visibility = 'visible'
-  list.style.visibility = 'visible';
+  //list.style.visibility = 'visible';
   // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
-
   // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
-  // check if the fuel level is less 10,000
+  let pilotStatus = document.getElementById("pilotStatus");
+  let copilotStatus = document.getElementById("copilotStatus");
+  pilotStatus.innerHTML =`Pilot ${pilotValue} is ready for launch`;
+  copilotStatus.innerHTML = `CoPilot ${copilotValue} is ready for launch`;
+  // check if the fuel level is less 10,000  
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the fuelStatus to "Fuel level too low for launch"
+    let launchStatus = document.getElementById("launchStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    fuelStatus.innerHTML = 'Fuel status good for launch!';
+    cargoStatus.innerHTML = 'Cargo status is good for launch!';
+    if (fuelLevelValue < 10000) {
+      list.style.visibility = 'visible';
+      launchStatus.innerHTML = 'Shuttle not ready for launch';
+      launchStatus.style.color = 'red';
+      fuelStatus.innerHTML = 'Fuel level too low for launch';    
+    }
 
+    if () //
     // check if the cargo level is more than 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the cargoStatus to "Cargo level too high for launch"
