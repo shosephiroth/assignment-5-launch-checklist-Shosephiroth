236a2547afac25de3045b05e089e40476fa06074
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-03 20:44:00 -0400

    got the code working! still needs a lil love

--- scriptHelper.js
+++ scriptHelper.js
@@ -69,6 +69,13 @@ function formSubmission(
   // alert user that they need to fill out all the fields alert('message')
   // check if fuelLevelValue and cargoLevelValue are not numbers
   // alert the user that must enter valid input
+  let pilotStatus = document.getElementById("pilotStatus");
+  pilotStatus.innerHTML = `Pilot ${pilotValue} is ready for launch`;
+  let copilotStatus = document.getElementById("copilotStatus");
+  copilotStatus.innerHTML = `CoPilot ${copilotValue} is ready for launch`;
+  let launchStatus = document.getElementById("launchStatus").textContent;
+  fuelLevelValue = Number(document.getElementsByName("fuelLevel")[0].value);
+  cargoLevelValue = Number(cargoLevelValue);
 
   if (
     validateInput(pilotValue) === "Empty" ||
@@ -87,20 +94,14 @@ function formSubmission(
     //list.style.visibility = 'visible';
     // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
     // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
-    let pilotStatus = document.getElementById("pilotStatus");
-    pilotStatus.innerHTML = `Pilot ${pilotValue} is ready for launch`;
-    let copilotStatus = document.getElementById("copilotStatus");
-    copilotStatus.innerHTML = `CoPilot ${copilotValue} is ready for launch`;
-    let launchStatus = document.getElementById("launchStatus");
-    let fuelStatus = document.getElementById("fuelStatus");
-    let cargoStatus = document.getElementById("cargoStatus");
+
     fuelStatus.innerHTML = "Fuel status good for launch!";
     cargoStatus.innerHTML = "Cargo status is good for launch!";
     // check if the fuel level is less 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the fuelStatus to "Fuel level too low for launch"
 
-    if (Number(fuelLevelValue) < 10000) {
+    if (fuelLevelValue < 10000) {
       list.style.visibility = "visible";
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
@@ -110,7 +111,7 @@ function formSubmission(
     // check if the cargo level is more than 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the cargoStatus to "Cargo level too high for launch"
-    if (Number(cargoLevelValue) > 10000) {
+    if (cargoLevelValue > 10000) {
       list.style.visibility = "visible";
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
@@ -119,7 +120,7 @@ function formSubmission(
 
     // if both fuel and cargo are good
     // change the launchStatus to "Shuttle is Ready for Launch" and color to green
-    if (Number(fuelLevelValue) < 10000 && Number(cargoLevelValue) > 10000) {
+    if (fuelLevelValue < 10000 && Number(cargoLevelValue) > 10000) {
       launchStatus.innerHTML = "Shuttle is Ready for Launch";
       launchStatus.style.color = "green";
     }
@@ -143,7 +144,16 @@ function pickPlanet(planets) {
   // randomly pick a planet from the array
   // Math random for index
   let planet = planets[Math.floor(Math.random() * planets.length)];
-  return planet;
+  //return planet;
+  addDestinationInfo(
+    document,
+    planet.name,
+    planet.diameter,
+    planet.star,
+    planet.distance,
+    planet.moons,
+    planet.image
+  );
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
